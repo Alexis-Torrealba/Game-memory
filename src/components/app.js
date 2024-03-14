98dadaa5938821b103b78c7d5e4f7077006694b8
Alexis-Torrealba <alexistorrealba1996@gmail.com>
2024-03-14 11:16:05 -0400

    Game responsive

--- src/components/app.js
+++ src/components/app.js
@@ -10,8 +10,6 @@ let numeros = [1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7, 8, 8];
 numeros = numeros.sort(() => {
 	return Math.random() - 0.5;
 });
-//mostaramos por consola para verificar que es cierto
-console.log(numeros);
 
 //Declaración de variables para la funionabilidad de la tarjeta
 let tarjetasDestapadas = 0;
